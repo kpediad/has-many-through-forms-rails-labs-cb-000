3fedee2f43be963c72006021fad424905c50673b
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-05 16:20:24 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['name'].blank? || attributes['email'].blank? }
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
   def user_attributes=(user_attributes)
-    user_attributes.values.each do |user_attribute|
-      user = User.find_or_create_by(user_attribute)
+    #user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attributes)
+    #  byebug
       self.user = user
-    end
+    #end
   end
 
 end
